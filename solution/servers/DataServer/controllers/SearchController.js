111ea2e976bd5a6f7fd566228fb1fe5993a5aaba
st214023 <matteo.tomaselli449@edu.unito.it>
2025-05-13 17:18:44 +0200

    Searchbar fixata e funzionante

--- solution/servers/DataServer/controllers/SearchController.js
+++ solution/servers/DataServer/controllers/SearchController.js
@@ -11,41 +11,37 @@ exports.search = async (req, res) => {
         const likePattern = `%${searchTerm}%`;
 
         const moviesQuery = `
-            SELECT id, name, year, tagline, description, minute, rating
-            FROM movies
-            WHERE name ILIKE $1 OR description ILIKE $2 OR tagline ILIKE $3
-            ORDER BY rating IS NULL, rating DESC
-            LIMIT 20
+            SELECT m.id, m.name, m.rating, p.link AS poster_link
+            FROM movies m
+            LEFT JOIN posters p ON m.id = p.id_movie
+            WHERE m.name ILIKE $1
+            ORDER BY m.rating IS NULL, m.rating DESC
+            LIMIT 100
         `;
 
         const actorsQuery = `
-            SELECT id, name, role, id_movie
+            SELECT DISTINCT ON (name) id, name
             FROM actors
-            WHERE name ILIKE $1 OR role ILIKE $2
-            LIMIT 20
+            WHERE name ILIKE $1
+            LIMIT 100
         `;
 
         const [moviesResults, actorsResults] = await Promise.all([
-            pg.query(moviesQuery, [likePattern, likePattern, likePattern]),
-            pg.query(actorsQuery, [likePattern, likePattern])
+            pg.query(moviesQuery, [likePattern]),
+            pg.query(actorsQuery, [likePattern])
         ]);
 
         const formattedMovies = moviesResults.rows.map(movie => ({
             id: movie.id,
             name: movie.name,
-            year: movie.year || 'Anno sconosciuto',
             type: 'movie',
-            description: movie.description || 'No description available',
-            tagline: movie.tagline || '',
-            rating: movie.rating || 'N/A'
-
-
+            rating: movie.rating || 'N/A',
+            posterLink: movie.poster_link || '/images/default-movie.jpg'
         }));
 
         const formattedActors = actorsResults.rows.map(actor => ({
             id: actor.id,
             name: actor.name,
-            role: actor.role || 'Ruolo sconosciuto',
             movieId: actor.id_movie,
             type: 'actor'
         }));
